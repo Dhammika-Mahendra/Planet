a9734cd9f8e62605f08d47f468df75491ff1b6a7
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-04-11 17:05:31 +0530

    Basic planetary structure

--- src/Screen.js
+++ src/Screen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect,useRef,useContext} from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Contxt from './Contx';
 
 export default function Screen() {
@@ -11,40 +12,109 @@ export default function Screen() {
   const ref = useRef();
 
   useEffect(()=>{
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, (window.innerWidth*0.8)/window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
 
-    renderer.setSize(window.innerWidth*0.8, window.innerHeight);
-    ref.current.appendChild(renderer.domElement);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, (window.innerWidth*0.8) / window.innerHeight, 0.1, 1000 );
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
+const directionalLight = new THREE.PointLight( 0xffffff,10,15 );
+scene.add( directionalLight );
+directionalLight.position.x=0
+directionalLight.position.y=0
 
-    scene.add(cube)
-    camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth*0.8, window.innerHeight );
+ref.current.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
 
-    let animationFrameId;
-    const animate = function () {
-      animationFrameId = requestAnimationFrame(animate);
+
+//Earth------------------------------------------------------------------
+const geometry = new THREE.SphereGeometry( 2,32,32 );
+const material = new THREE.MeshStandardMaterial( { map:new THREE.TextureLoader().load('earth.jpg') } );
+const earth = new THREE.Mesh( geometry, material );
+scene.add( earth );
+earth.position.x=6
+
+//Equator----------------------------------------------------------------
+const curve = new THREE.EllipseCurve(
+	0,  0,            // ax, aY
+	2.2,  2.2,         // xRadius, yRadius
+	0,  2 * Math.PI,  // aStartAngle, aEndAngle
+	false,            // aClockwise
+	0                 // aRotation
+);
+
+const points2 = curve.getPoints( 50 );
+const geometry5 = new THREE.BufferGeometry().setFromPoints( points2 );
+const material5 = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+const ellipse = new THREE.Line( geometry5, material5 );
+ellipse.rotateX(1.5708)
+earth.add(ellipse)
+earth.rotateZ(-0.4101524)
+
+//Sun
+const geometry2 = new THREE.SphereGeometry( 2,32,32 );
+const material2 = new THREE.MeshBasicMaterial( { map:new THREE.TextureLoader().load('sun.jpg') } );
+const sun = new THREE.Mesh( geometry2, material2 );
+scene.add( sun );
+//earth.add(wireEarth)
+
+//Rotation axis of earth-------------------------------------------------------
+const material3 = new THREE.LineBasicMaterial({
+	color: 0x0000ff
+});
+const points = [];
+points.push( new THREE.Vector3( 0,3,0 ) );
+points.push( new THREE.Vector3( 0,-3,0 ) );
+
+const geometry3 = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry3, material3 );
+earth.add( line );
+
+//Earth trajectory
+const curve2 = new THREE.EllipseCurve(
+	0,  0,            // ax, aY
+	8, 	6,             // xRadius, yRadius
+	0,  2 * Math.PI,  // aStartAngle, aEndAngle
+	false,            // aClockwise
+	0                 // aRotation
+);
+const points3 = curve2.getPoints( 50 );
+const geometry6 = new THREE.BufferGeometry().setFromPoints( points3 );
+const material6 = new THREE.LineBasicMaterial( { color: 0xffffff } );
+const ellipse2 = new THREE.Line( geometry6, material6 );
+ellipse2.rotateX(1.5708)
+scene.add(ellipse2)
+
+camera.position.z = 15;
+
+let t=0
+let animationId
+function animate() {
+    controls.update();
+    earth.rotateOnAxis(new THREE.Vector3(0,1,0),0.01745)
+	  animationId=requestAnimationFrame( animate );
+	  sun.rotation.y+=0.0025
+
+    t += 0.001;
+    if (t > 1) t = 0;
   
-      cube.rotation.x += (speed/1000);
-      cube.rotation.y += 0.01;
-  
-      renderer.render(scene, camera);
-    };
-  
-    animate()
+    let point = curve2.getPoint(t);
+	  let vector = new THREE.Vector3(point.x, point.y, point.z)
+	  vector.applyAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2);
 
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-      if (ref.current.children.length !== 0) {
-        ref.current.removeChild(renderer.domElement);
-      }
-    };
+    earth.position.copy(vector)
 
-  },[speed])
+	  renderer.render( scene, camera );
+}
+animate();
+
+  return () => {
+    cancelAnimationFrame(animationId); // Cancel the animation
+    ref.current.removeChild(renderer.domElement); // Remove the canvas from the div
+  };
+
+  },[])
 
   return (
     <div ref={ref}>
