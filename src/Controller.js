864e5874869ae7f26a67e30bc59b5da39b237dd4
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-07-04 11:22:49 +0530

    Help box finished

--- src/Controller.js
+++ src/Controller.js
@@ -10,6 +10,7 @@ import { Box, Button, Paper, Typography } from '@mui/material'
 import HelpIcon from '@mui/icons-material/Help';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react'
+import instructions from './instructions.png'
 
 export default function Controller() {
   const [help,setHelp]=useState(false)
@@ -26,9 +27,11 @@ export default function Controller() {
         <HelpIcon sx={{cursor:'pointer',alignSelf:'end','&:hover':{color:'blue'},mr:'20px'}} onClick={()=>setHelp(true)}></HelpIcon>
 
         {/* ---- help box ------ */}
-        {help?<Paper sx={{position:'fixed',width:'90vw',height:'90vh',top:'5vh',left:'5vh',backgroundColor:'#efefef',zIndex:'40'}}>
-            <Box sx={{height:'15px',p:'4px',display:'flex',justifyContent:'end',alignItems:'center'}}>
-            <CloseIcon sx={{cursor:'pointer'}} onClick={()=>setHelp(false)}></CloseIcon>
+        {help?<Paper sx={{position:'fixed',width:'90vw',height:'90vh',top:'5vh',left:'5vh',backgroundColor:'#efefef',zIndex:'40',display:'flex',flexDirection:'column',alignItems:'center',border:'1px solid black'}}>
+            <Box sx={{height:'15px',p:'4px',display:'flex',justifyContent:'end',alignItems:'center',alignSelf:'end'}}>
+            <CloseIcon sx={{cursor:'pointer','&:hover':{color:'blue'}}} onClick={()=>setHelp(false)}></CloseIcon>
+            </Box>
+            <Box sx={{height:'95%',width:'95%',backgroundImage:`url(${instructions})`,backgroundRepeat:'no-repeat',backgroundSize:'contain'}}>
             </Box>
         </Paper>:''}
 
